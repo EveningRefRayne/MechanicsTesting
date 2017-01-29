8e082cdd78777984b98b42ef0b0b0a1149fd65d5
ScottT4ylor <Scott.T4ylor@gmail.com>
2017-01-29 16:41:25 -0500

    cleaned up scripts
    added descriptions of how they work

--- Assets/CameraControl.cs
+++ Assets/CameraControl.cs
@@ -4,6 +4,20 @@ using UnityEngine;
 using System.Windows.Forms;
 using System.Drawing;
 
+
+/// <summary>
+/// Controls camera rotation
+/// Requires cameraAnchorX for the object rotating on the X axis
+/// Requires cameraAnchorZ for the object rotating on the Z axis
+/// 
+/// Parent cameraAnchorX over cameraAnchorZ over camera.
+/// center cameraAnchorZ on cameraAnchorX
+/// 
+/// For mouselook, center camera on cameraAnchorZ.
+/// For orbit camera, move cameraAnchorX away from the camera.
+/// 
+/// </summary>
+
 public class CameraControl : MonoBehaviour {
     
     //## - Means set in the inspector
@@ -22,32 +36,54 @@ public class CameraControl : MonoBehaviour {
 
     private Quaternion cameraAnchorTargetRotX;
     private Quaternion cameraAnchorTargetRotZ;
-    private bool lockCursor = false;
     private bool MLMode = true;
 
+    public bool setLockPos = true;
+    private Point mouseLockPos;
 
     public void Awake()
     {
         //Sets the character and camera's target rotation to their current one when the script starts running.
         cameraAnchorTargetRotZ = cameraAnchorZ.localRotation;
         cameraAnchorTargetRotX = cameraAnchorX.localRotation;
-        //setCursorLock(lockCursor);
     }
+
+
+    public void Update()
+    {
+        //on middle mouse click, makes the mouse invisible, locks it in place, and calls to rotate the camera
+        if (Input.GetAxis("MiddleClick") == 1)
+        {
+            if (setLockPos)
+            {
+                mouseLockPos = System.Windows.Forms.Cursor.Position;
+                setLockPos = false;
+                UnityEngine.Cursor.visible = false;
+            }
+            System.Windows.Forms.Cursor.Position = mouseLockPos;
+            rotateCam();
+        }
+        else
+        {
+            setLockPos = true;
+            UnityEngine.Cursor.visible = true;
+        }
+    }
+
 
     private void rotateCam()
     {
         //Updates every frame. If the player's in mouselook mode, it runs the function that does the work and checks to see if they want to lock/unlock the mouse.
         if (MLMode)
         {
-            if (lockCursor) lookRotation();
-            //updateCursorLock();
+            lookRotation();
         }
     }
 
 
     private void lookRotation()
     {
-        //This is what runs the mouselook, moving the character and camera transforms to follow the mouse. Called every Update.
+        //This is what runs the mouselook, rotating the camera anchors.
         float yRot = Input.GetAxis("Mouse X") * XSensitivity * invertX;
         float xRot = Input.GetAxis("Mouse Y") * YSensitivity * invertY;
 
@@ -71,7 +107,7 @@ public class CameraControl : MonoBehaviour {
 
     private Quaternion clampRotationAroundXAxis(Quaternion q)
     {
-        //This is used by lookRotation to clamp the axis to vertical up and down so the player can't do terrible things with the camera.
+        //This clamps the vertical axis so the player can't do terrible things with the camera.
         q.x /= q.w;
         q.y /= q.w;
         q.z /= q.w;
@@ -84,41 +120,13 @@ public class CameraControl : MonoBehaviour {
         return q;
     }
 
-    /*private void updateCursorLock()
-    {
-        //this checks every update if the player hit escape to unlock the mouse, or clicked back in.
-        if (Input.GetKeyUp(KeyCode.Escape))
-        {
-            setCursorLock(false);
-        }
-        else if (Input.GetMouseButtonUp(0))
-        {
-            setCursorLock(true);
-        }
-    }*/
 
 
-    /*private void setCursorLock(bool value)
-    {
-        //this is called internally to do the locking and unlocking when you want to lock or unlock the cursor.
-        lockCursor = value;
-        if (lockCursor)
-        {
-            UnityEngine.Cursor.lockState = CursorLockMode.Locked;
-            UnityEngine.Cursor.visible = false;
-        }
-        else
-        {
-            UnityEngine.Cursor.lockState = CursorLockMode.None;
-            UnityEngine.Cursor.visible = true;
-        }
-    }*/
 
     public void setMLMode(bool value)
     {
-        //This is an external function to be called by other scripts to change into and out of mouselook mode entirely.
+        //This is a function to be called by other scripts to enable/disable rotation
         MLMode = value;
-        //setCursorLock(MLMode);
     }
 
     //These two are for inverting the X and Y movement.
@@ -130,42 +138,4 @@ public class CameraControl : MonoBehaviour {
     {
         invertY *= -1;
     }
-
-
-
-
-
-
-
-
-
-
-
-
-    public bool setLockPos = true;
-    private Point mouseLockPos;
-
-    public void Update()
-    {
-
-        
-        if(Input.GetAxis("MiddleClick") == 1)
-        {
-            if(setLockPos == true)
-            {
-                mouseLockPos = System.Windows.Forms.Cursor.Position;
-                setLockPos = false;
-                lockCursor = true;
-                UnityEngine.Cursor.visible = false;
-            }
-            System.Windows.Forms.Cursor.Position = mouseLockPos;
-            rotateCam();
-        }
-        else
-        {
-            lockCursor = false;
-            setLockPos = true;
-            UnityEngine.Cursor.visible = true;
-        }
-    }	
 }
--- Assets/SimpleMove.cs
+++ Assets/SimpleMove.cs
@@ -2,6 +2,12 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+
+/// <summary>
+/// Simple movement script will move the object based on key input, in the direction the object is facing.
+/// Requires XMove, YMove, and ZMove to be set up in the input manager.
+/// </summary>
+
 public class SimpleMove : MonoBehaviour {
     public Vector3 mov;
     public float speed = 2;
